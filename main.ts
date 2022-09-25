dfe8c2900cec80c53f31a3ab0225fcdb723c36e8
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2022-09-25 13:58:03 +0200

    add starter code

---
+++ main.ts
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+import { game } from "./src/tictactoe";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+game.render();
+
+rl.prompt();
+
+rl.on('line', (line: string) => {
+    if (line.toLowerCase() === "exit") {
+        process.exit(0);        
+    }
+    
+    game.render();
+
+    game.command(line.trim());
+    
+    rl.prompt();
+}).on('close', () => {
+    console.log('Bye!');
+    process.exit(0);
+});
---
+++ src/tictactoe.ts
@@ -0,0 +1,15 @@
+
+
+class Game {
+    public render() {
+        console.log('TIC - TAC - TOE')
+    }
+
+    public command(command: string) {
+        // Perform game logic
+    }
+}
+
+const game = new Game();
+
+export { game };
