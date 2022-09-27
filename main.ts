46ea8123459afc8c68afe27e4f432858886313e5
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-09-27 20:09:47 +0200

    first init

--- main.ts
+++ main.ts
@@ -1,27 +1,27 @@
-const readline = require('readline');
+const readline = require("readline");
 
 import { game } from "./src/tictactoe";
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 game.render();
 
 rl.prompt();
 
-rl.on('line', (line: string) => {
-    if (line.toLowerCase() === "exit") {
-        process.exit(0);        
-    }
-    
-    game.render();
-
-    game.command(line.trim());
-    
-    rl.prompt();
-}).on('close', () => {
-    console.log('Bye!');
+rl.on("line", (line: string) => {
+  if (line.toLowerCase() === "exit") {
     process.exit(0);
+  }
+
+  game.render();
+
+  game.command(line.trim());
+
+  rl.prompt();
+}).on("close", () => {
+  console.log("Bye!");
+  process.exit(0);
 });
--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -1,15 +1,61 @@
-
-
 class Game {
-    public render() {
-        console.log('TIC - TAC - TOE')
+  public render() {
+    console.log("TIC - TAC - TOE");
+  }
+
+  public command(command: string) {
+    // Perform game logic
+    if (command === PlayerSigns.O) {
+      command = "O";
+    } else if (command === PlayerSigns.X) {
+      command = "X";
+    } else {
+      command = "You should enter valid expression!";
     }
 
-    public command(command: string) {
-        // Perform game logic
-    }
+    const cells: string[][] = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ];
+
+    console.log(
+      " | " + cells[0][0] + " | " + cells[0][1] + " | " + cells[0][2] + " | "
+    );
+    console.log(
+      " | " + cells[1][0] + " | " + cells[1][1] + " | " + cells[1][2] + " | "
+    );
+    console.log(
+      " | " + cells[1][0] + " | " + cells[2][1] + " | " + cells[2][2] + " | "
+    );
+
+    console.log(command);
+  }
 }
 
 const game = new Game();
+
+enum PlayerSigns {
+  O = "O",
+  X = "X",
+}
+
+// for (let i = 0; i < cells.length; i++) {
+//   for (let j = 0; j < cells[i].length; j++) {
+//     console.log(cells[i, j]);
+//   }
+// }
+
+class Player {}
+
+//get random number
+// const getRandomInteger = (min: number, max: number): number => {
+//   min = Math.ceil(1);
+//   max = Math.floor(9);
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// };
+
+let computer = new Player();
+let human = new Player();
 
 export { game };
